21481cc9f3d0cf8c6805c7607aebb4bc6e382c62
ayubdev <ayubgardy898@gmail.com>
2022-08-22 11:33:36 +0300

    Done the assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const superbowlWin = (superbowls) => {
+    const superbowlW = superbowls.find(superbowl => superbowl.result === 'W');
+    
+    if (!superbowlW) {
+        return undefined
+    }
+
+    return superbowlW.year;
+}
